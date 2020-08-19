0de6ef763e351132b0c224ae99eebd628981bad0
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-19 20:50:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
-end
+ 
+ array.each do |i|
+      i ** 2
+    end
+    numbers = [1,2,3]
+ 
+square_array(numbers)
+# => [1,4,9])
+ 
+new_numbers = [9,10,16,25]
+ 
+square_array(new_numbers)
+  
+end 
